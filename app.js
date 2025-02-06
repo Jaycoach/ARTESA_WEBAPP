b149ee43785a5b1c43b3dd2f0b52f39b4adc1a3d
Jaycoach <jaycoach@hotmail.com>
2025-02-07 00:54:15 +0100

    Actualizacion Rutas y Swagger

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const app = express();
+const userRoutes = require('./src/routes/userRoutes');
+const authRoutes = require('./src/routes/authRoutes');
+const PORT = process.env.PORT || 3000;
 
 // Middleware para Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+app.use('/api', userRoutes);
+
+app.use('/api/auth', authRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
-const userRoutes = require('./src/routes/userRoutes');
-app.use('/api', userRoutes);
-
-const authRoutes = require('./src/routes/authRoutes');
-app.use('/api/auth', authRoutes);
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,13 +2,12 @@ const pool = require('../config/db');
 
 const getUsers = async (req, res) => {
   try {
+    console.log('Iniciando consulta de usuarios');
     const { rows } = await pool.query('SELECT * FROM users');
+    console.log('Usuarios encontrados:', rows.length);
     res.json(rows);
   } catch (err) {
+    console.error('Error al obtener usuarios:', err);
     res.status(500).json({ error: err.message });
   }
-};
-
-module.exports = {
-  getUsers,
 };
