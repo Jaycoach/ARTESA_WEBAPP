830bb7ad402629fd82ecc21784990d99745cd86e
lukoard <lukasdariam@gmail.com>
2025-03-16 00:58:32 -0500

    Fix doble enlace Nuevo Pedido

--- src/views/frontend/LoginArtesa/src/Components/Dashboard/Pages/Orders/Orders.jsx
+++ src/views/frontend/LoginArtesa/src/Components/Dashboard/Pages/Orders/Orders.jsx
@@ -134,7 +134,7 @@ const Orders = () => {
               className={`${
                 activeTab === 'new'
                   ? 'border-indigo-500 text-indigo-600'
-                  : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                  : 'hidden border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
               } w-1/2 py-4 px-1 text-center border-b-2 font-medium text-sm`}
             >
               Nuevo Pedido
