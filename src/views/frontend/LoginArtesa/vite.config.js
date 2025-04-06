e02308b3b4ee4dc6d3fbfedb6de235e112f67c08
Jaycoach <jaycoach@hotmail.com>
2025-04-06 23:04:30 +0100

    Fixes Home

--- src/views/frontend/LoginArtesa/vite.config.js
+++ src/views/frontend/LoginArtesa/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from 'tailwindcss'
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
